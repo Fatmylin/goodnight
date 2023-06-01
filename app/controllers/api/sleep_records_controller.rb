8093f172da5521a177d06c48c54ea1710a0b9d16
Jimmy Hsiao <jimmy.h@incubit.co.jp>
2023-06-01 15:24:30 +0800

    add: sleep records controller

---
+++ app/controllers/api/sleep_records_controller.rb
@@ -0,0 +1,35 @@
+module API
+  class SleepRecordsController < ApplicationController
+    def index
+      render json: { sleep_records: current_user.sleep_records.valid.order(created_at: :desc) }
+    end
+
+    def create
+      sleep_record = current_user.sleep_records.create
+      render json: { id: sleep_record.id }
+    end
+
+    def update
+      sleep_record = current_user.sleep_records.find(params[:id])
+      if sleep_record.update(sleep_record_params)
+        render json: { id: sleep_record.id }
+      else
+        render json: { errors: sleep_record.errors.full_messages.join(",") }
+      end
+    end
+
+    private
+
+    def sleep_record_params
+      if (params[:clock_action] == "start")
+        {
+          start_at: Time.now
+        }
+      else
+        {
+          finish_at: Time.now
+        }
+      end
+    end
+  end
+end
